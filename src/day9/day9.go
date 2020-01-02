9a9380d4971a792181fd38cf32996d9d6911d1a9
Paul Caporn <paul.caporn@bbc.co.uk>
2020-01-02 16:59:56 +0000

    Added part2 input

--- src/day9/day9.go
+++ src/day9/day9.go
@@ -10,4 +10,8 @@ func main() {
 	go p.ProcessProgram()
 	p.Input <- 1
 	fmt.Printf("Part 1: %v\n", <-p.Output)
+	p = intcomp.ReadProgram("input")
+	go p.ProcessProgram()
+	p.Input <- 2
+	fmt.Printf("Part 2: %v\n", <-p.Output)
 }
