f1f5de7d5e9615b181226defb5efd1acf282d1eb
Paul Caporn <paul.caporn@bbc.co.uk>
2020-01-14 00:20:24 +0000

    Missing files

---
+++ src/render/render.go
@@ -0,0 +1,34 @@
+package render
+
+import (
+	"strings"
+)
+
+func GenerateBoardHtml(board [][]int64, chars map[int64]string) string {
+	return generateBoard(board, chars, "<div><pre>", "</pre></div>")
+
+}
+
+func GenerateBoardText(board [][]int64, chars map[int64]string) string {
+	return generateBoard(board, chars, "", "\n")
+}
+
+func generateBoard(board [][]int64, chars map[int64]string, rowStart, rowEnd string) string {
+	var output strings.Builder
+	for y := 0; y < len(board); y++ {
+		if rowStart != "" {
+			output.WriteString(rowStart)
+		}
+		for x := 0; x < len(board[y]); x++ {
+			entry := chars[board[y][x]]
+			if entry == "" {
+				entry = " "
+			}
+			output.WriteString(entry)
+		}
+		if rowEnd != "" {
+			output.WriteString(rowEnd)
+		}
+	}
+	return output.String()
+}
